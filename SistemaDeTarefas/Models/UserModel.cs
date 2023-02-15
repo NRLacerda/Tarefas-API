641ed9f39fcb4532992ca9479aa8a31440cf774f
Nicolas <Nicolas@DESKTOP-TOUCHLA>
2023-02-14 23:01:53 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmailConfirmed { get; set; }
+
+    }
+}
