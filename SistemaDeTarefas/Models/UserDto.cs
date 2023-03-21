6757a6f47c72af63475cd4d842d2b4a5a2eba647
NRLacerda <nrochalacerda@gmail.com>
2023-03-21 17:09:26 -0300

    att user log

---
+++ SistemaDeTarefas/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
