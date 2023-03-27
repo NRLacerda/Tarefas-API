fbab5d1f02d09fc97b190c625fc13c99e5fda45f
Nicolas <nrochalacerda@gmail.com>
2023-03-26 23:03:03 -0300

    att

--- SistemaDeTarefas/Controller/AuthController.cs
+++ SistemaDeTarefas/Controller/AuthController.cs
@@ -11,17 +11,35 @@ namespace SistemaDeTarefas.Controller
     {
         // intancia objeto de User
         public static User user = new User(); 
-        // 12:45
 
         [HttpPost("Register")]
+        // recebe UserDto um modelo que contem Username e Password para registralos
         public async Task<ActionResult<User>> Register (UserDto request)
         {
-            // chama o metodo que hasheia a password 
+            // esse request.Password é a senha passada no header da requisição
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
             user.PasswordHash= passwordHash;
             user.PasswordSalt= passwordSalt;
             user.Username = request.Username;
             return Ok(user); //retorna usuario com nome senha hash e salt hash
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult<string>> Login (UserDto request)
+        {
+            // compara usuario e depois senhas
+            if (user.Username != request.Username)
+            {
+                return BadRequest("Usuário não encontrado");
+            }
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Senha incorreta");
+            }
+            else
+            {
+                return Ok("TOKEN THERE");
+            }
+
         }
         private void CreatePasswordHash (string password, out byte[] passwordHash, out byte[] PasswordSalt)
         {
@@ -29,7 +47,21 @@ namespace SistemaDeTarefas.Controller
             using (var hmac = new HMACSHA512())
             {
                 PasswordSalt = hmac.Key;
+                // cria o password hashado
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                /*
+                   a senha vem como string e é convertida em array de bytes para ser comparada com outro array de bytes
+                   que é o passwordHash = computedHash
+                 */
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                // retorna true se o passwordhash for igual ao computed hash (senha hashada que o usuario enviou)
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
 
